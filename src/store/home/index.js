aeb8ec23a4fc58d138957df10fd33d21aae7b92c
chcorophyll <chcorophyll@gmail.com>
2024-01-19 15:53:40 +0800

    categorylist data done

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,6 +1,24 @@
-const state = {};
-const actions = {};
-const mutations = {};
+import { reqgetCategoryList } from "@/api";
+
+const state = {
+    categoryList: [],
+};
+
+const actions = {
+    async getCategoryList({commit}) {
+        let result = await reqgetCategoryList();
+        if (result.code == 200) {
+            commit("GETCATEGORYLIST", result.data);
+        }
+    },
+};
+
+const mutations = {
+    GETCATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList;
+    },
+};
+
 const getters = {};
 
 export default {
