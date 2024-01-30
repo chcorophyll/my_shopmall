7c4c75db8d55f711702ddf5b83dedc2d1ee0839d
chcorophyll <chcorophyll@gmail.com>
2024-01-30 09:46:23 +0800

    search component  static page done and router scroll behavior add

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,15 @@ import Home from "@/pages/Home";
 import Search from "@/pages/Search";
 import Register from "@/pages/Register";
 import Login from "@/pages/Login";
+import Detail from "@/pages/Detail";
 
 
 export default [
+  {
+    path: "/detail/:skuId",
+    component: Detail,
+    meta: {isShowFooter: true},
+  },
   {
     path: "/home",
     component: Home,
