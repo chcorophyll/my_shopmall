bc7eb8399a1906e752e0530d94538430ebb1a6c1
chcorophyll <chcorophyll@gmail.com>
2024-02-08 11:09:36 +0800

    router guard add logout case

--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,19 @@ import store from "@/store";
 Vue.use(VueRouter);
 
 let originPush = VueRouter.prototype.push;
-VueRouter.prototype.push = function(location, resolve, reject) {
+VueRouter.prototype.push = function (location, resolve, reject) {
   if (resolve & reject) {
     originPush.call(this, location, resolve, reject);
   } else {
-    originPush.call(this, location, () => {}, () => {});
+    originPush.call(this, location, () => { }, () => { });
   }
 };
 let originReplace = VueRouter.prototype.replace;
-VueRouter.prototype.replace = function(location, resolve, reject) {
+VueRouter.prototype.replace = function (location, resolve, reject) {
   if (resolve & reject) {
     originReplace.call(this, location, resolve, reject);
   } else {
-    originReplace.call(this, location, () => {}, () => {});
+    originReplace.call(this, location, () => { }, () => { });
   }
 };
 
@@ -28,41 +28,46 @@ VueRouter.prototype.replace = function(location, resolve, reject) {
 
 //对外暴露VueRouter类的实例
 let router = new VueRouter({
-    //配置路由
-    //第一:路径的前面需要有/(不是二级路由)
-    //路径中单词都是小写的
-    //component右侧V别给我加单引号【字符串：组件是对象（VueComponent类的实例）】
-    routes,
-    //滚动行为
-    scrollBehavior(to, from, savedPosition) {
-      //返回的这个y=0，代表的滚动条在最上方
-      return { y: 0 };
-    },
-  });
+  //配置路由
+  //第一:路径的前面需要有/(不是二级路由)
+  //路径中单词都是小写的
+  //component右侧V别给我加单引号【字符串：组件是对象（VueComponent类的实例）】
+  routes,
+  //滚动行为
+  scrollBehavior(to, from, savedPosition) {
+    //返回的这个y=0，代表的滚动条在最上方
+    return { y: 0 };
+  },
+});
 
-  router.beforeEach(async (to, from, next) => {
-    let token = store.state.user.token;
-    let name = store.state.user.userInfo.name;
-    if (token) {
-      if(to.path=="/login"||to.path=='/register') {
-        next('/home');
+router.beforeEach(async (to, from, next) => {
+  let token = store.state.user.token;
+  let name = store.state.user.userInfo.name;
+  if (token) {
+    if (to.path == "/login" || to.path == '/register') {
+      next('/home');
+    } else {
+      if (name) {
+        next();
       } else {
-        if (name) {
+        try {
+          await store.dispatch('getUserInfo');
           next();
-        } else {
-          try {
-            await store.dispatch('getUserInfo');
-            next();
-          } catch (error) {
-            await store.dispatch('logoutUser');
-            next('/login');
-          }
+        } catch (error) {
+          await store.dispatch('logoutUser');
+          next('/login');
         }
-        
       }
+
+    }
+  } else {
+    let toPath = to.path;
+    if (toPath.indexOf('/trade') != -1 || toPath.indexOf('/pay') != -1 || toPath.indexOf('/center') != -1) {
+      next('/login?redirect=' + toPath);
     } else {
       next();
     }
-  })
+  }
+})
 
-  export default router;
+export default router;
