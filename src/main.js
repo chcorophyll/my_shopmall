2dc43d50a338a428ec76793949b88b90e618e395
chcorophyll <chcorophyll@gmail.com>
2024-02-09 10:13:02 +0800

    add form validate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from "@/router";
 import store  from '@/store';
 import { Button,MessageBox} from 'element-ui';
+import "@/plugins/validate";
 
 
 Vue.config.productionTip = false
